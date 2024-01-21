ef6b55dfe82bf17d93561308e40e251053408983
Anthony Josué Rodríguez Camuendo <anthony.rodriguez.camuendo@udla.edu.ec>
2024-01-20 20:33:28 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/SedesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_.Models;
+
+namespace ProyectoFinal_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SedesController : ControllerBase
+    {
+        private readonly ProyectoFinal_Context _context;
+
+        public SedesController(ProyectoFinal_Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sedes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sede>>> GetSede()
+        {
+          if (_context.Sede == null)
+          {
+              return NotFound();
+          }
+            return await _context.Sede.ToListAsync();
+        }
+
+        // GET: api/Sedes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sede>> GetSede(int id)
+        {
+          if (_context.Sede == null)
+          {
+              return NotFound();
+          }
+            var sede = await _context.Sede.FindAsync(id);
+
+            if (sede == null)
+            {
+                return NotFound();
+            }
+
+            return sede;
+        }
+
+        // PUT: api/Sedes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSede(int id, Sede sede)
+        {
+            if (id != sede.SedeID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sede).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SedeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sedes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Sede>> PostSede(Sede sede)
+        {
+          if (_context.Sede == null)
+          {
+              return Problem("Entity set 'ProyectoFinal_Context.Sede'  is null.");
+          }
+            _context.Sede.Add(sede);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSede", new { id = sede.SedeID }, sede);
+        }
+
+        // DELETE: api/Sedes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSede(int id)
+        {
+            if (_context.Sede == null)
+            {
+                return NotFound();
+            }
+            var sede = await _context.Sede.FindAsync(id);
+            if (sede == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sede.Remove(sede);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SedeExists(int id)
+        {
+            return (_context.Sede?.Any(e => e.SedeID == id)).GetValueOrDefault();
+        }
+    }
+}
